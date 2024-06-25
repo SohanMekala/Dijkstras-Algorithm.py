187dff161f1bb0ef34d3fc8cd28664862f9fb73d
Sohan Mekala <80227230+SohanMekala@users.noreply.github.com>
2024-06-24 23:23:19 -0500

    Update Dijkstras-Algorithm.py

--- Dijkstras-Algorithm.py
+++ Dijkstras-Algorithm.py
@@ -1,3 +1,21 @@
+nodes = nodes = ['A', 'B', 'C', 'D', 'E', 'F']
+
+edges = {
+    ('A', 'B') : 2,
+    ('A', 'D') : 8,
+    ('B', 'D') : 5,
+    ('B', 'E') : 6,
+    ('D', 'E') : 3,
+    ('D', 'F') : 2,
+    ('E', 'F') : 1,
+    ('E', 'C') : 9,
+    ('F', 'C') : 1,
+}
+
+start = 'A'
+end = 'C'
+
+'''
 nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
 edges = {
@@ -18,38 +36,79 @@ start = 'A'
 end = 'B'
 
 '''
-1. constantly update estimates
-2. choose next vertex
-'''
 
-def minimum(dict):
-    min_key = list(dict.keys())[0]
-    for i in list(dict.keys())[1:]:
-        if dict[i] < dict[min_key]:
-            min_key = i
-    return(min_key)
+unvisited = []
 
-def dijkstra(nodes, edges, start, end):
-    unexplored = {node : float('inf') for node in nodes}
-    unexplored[start] = 0
-    while len(unexplored) != 0:
-        explore = minimum(unexplored)
-        if explore == end:
-            break
-        else:
-            for path in edges.items():
-                if path[0][0] == explore:
-                    if path[0][1] in unexplored.keys():
-                        check_time = unexplored[path[0][0]] + path[1]
-                        if check_time < unexplored[path[0][1]]:
-                            unexplored[path[0][1]] = check_time
-                elif path[0][1] == explore:
-                    if path[0][0] in unexplored.keys():
-                        check_time = unexplored[path[0][1]] + path[1]
-                        if check_time < unexplored[path[0][0]]:
-                            unexplored[path[0][0]] = check_time
-            del unexplored[explore]
+shortestDist = {} #shortest dist between start and any given node
+prevNode = {} #prev node, used for backtracking
 
-    return(unexplored[explore])
 
-print(dijkstra(nodes, edges, start, end))
+for node in nodes:
+    #setting all nodes to unvisted
+    unvisited.append(node)
+
+    #setting shortest dist of all nodes to infinity except the starting node, which is 0
+    if node == start:
+        shortestDist[node] = 0
+    else:
+        shortestDist[node] = float('inf')
+    
+    prevNode[node] = None
+
+workingNode = start
+
+while workingNode != end:
+    neighbors = {}
+
+    #identify neighbors and set shortest distance
+    for edge in edges:
+        if workingNode in edge:
+
+            neighbor = edge[1-edge.index(workingNode)] #works bcuz there is only two elements in edge tuple
+
+            if neighbor in unvisited:
+                
+                if shortestDist[workingNode]+edges[edge] < shortestDist[neighbor]:
+                    shortestDist[neighbor] = shortestDist[workingNode]+edges[edge]
+                    prevNode[neighbor] = workingNode
+
+    unvisited.remove(workingNode)
+
+    #greedy aproach to find closest node
+    closestNeighborDist = float('inf')
+    closestNeighbor = None
+
+    for neighbor in unvisited:
+        if shortestDist[neighbor] < closestNeighborDist:
+            closestNeighbor = neighbor
+            closestNeighborDist = shortestDist[neighbor]
+    
+    workingNode = closestNeighbor
+
+#backtracking
+path = []
+node = end
+while node is not None:
+    path.append(node)
+    node = prevNode[node]
+
+disp = ""
+
+for i in range(len(path)):
+    if(i!=len(path)-1):
+        disp+=path[i]
+        disp+=" -> "
+    else:
+        disp+=path[i]
+
+print("Shortest Distance from ",start," to ",end," is ",shortestDist[end])
+print(disp)
+
+
+
+
+
+    
+
+
+
